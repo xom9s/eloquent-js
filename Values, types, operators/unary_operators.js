252672d184fbe21d040fd1ebf1e4c39a7629d2f9
xom9s <xom9k321@gmail.com>
2024-09-13 07:20:33 +0300

    Values, Types, and Operators practice

---
+++ Values, types, operators/unary_operators.js
@@ -0,0 +1,48 @@
+console.log(typeof 4.5);
+// number
+console.log(-(10 - 2));
+// -8
+console.log(typeof "x");
+//string
+console.log("Garnet" != "Ruby");
+// → true
+console.log("Pearl" == "Amethyst");
+// → false
+console.log(NaN == NaN);
+// → false
+console.log(true && false);
+// → false
+console.log(true && true);
+// → true
+console.log(false || true);
+// → true
+console.log(false || false);
+// → false
+console.log(true ? 1 : 2);
+// → 1
+console.log(false ? 1 : 2);
+// → 2
+console.log(8 * null);
+// → 0
+console.log("5" - 1);
+// → 4
+console.log("5" + 1);
+// → 51
+console.log("five" * 2);
+// → NaN
+console.log(false == 0);
+// → true
+console.log(null == undefined);
+// → true
+console.log(null == 0);
+// → false
+console / log(null || "user");
+// → user
+console.log("Agnes" || "user");
+// → Agnes
+console.log(0 || 100);
+// → 100
+console.log(0 ?? 100);
+// → 0
+console.log(null ?? 100);
+// → 100
